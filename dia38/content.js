479a136c598be39dc5e367884022356f9774a0e5
Liz-Cei <marisbelsy1991@alumnos.cei.es>
2024-12-18 11:57:06 +0100

    cambios de apuntes

--- dia38/content.js
+++ dia38/content.js
@@ -1,24 +1,40 @@
 let users =[];
 const card = document.getElementById("card");
 
-// probando fetch
 function traerUsuarios(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(
         data => {
-            users = data 
-    
+            users = data;
+            console.log(users);
+            users.map(user => {
+                // Crear nuevos elementos para cada usuario
+                let h1 = document.createElement('h1');
+                let p = document.createElement('p');
+                let pCity = document.createElement('p');
+                let pCompany = document.createElement('p');
+                let div = document.createElement('div');
+                let fragmento = document.createDocumentFragment();
+
+                h1.textContent = `${user.name}`;
+                p.innerHTML = `<strong>email:</strong> ${user.email}`;
+                pCity.innerHTML = `<strong>ciudad:</strong> ${user.address.city}`;
+                pCompany.innerHTML = `<strong>compañia:</strong> ${user.company.name}`;
+                console.log(h1, p, pCity, pCompany);
+                h1.classList.add('Cards-title');
+                div.classList.add('Cards-card');
+                div.appendChild(h1);
+                div.appendChild(p);
+                div.appendChild(pCity);
+                div.appendChild(pCompany);
+                fragmento.appendChild(div);
+                card.appendChild(fragmento);
+            });
         }
     )
-
 }
-// funcion para crear un nuevo div 
-let fragmento = document.createDocumentFragment();
-
-const div = document.createElement('div');
-users.map(user =>  div.textContent += `nombre: ${user.name}`);
-fragmento.appendChild(div);
-document.body.appendChild(fragmento);
 
 traerUsuarios();
+
+
